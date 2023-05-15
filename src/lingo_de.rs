1751546328da75abac734b52a64083b88b20cebc
thalber <filefillman@gmail.com>
2023-05-15 20:26:13 +0400

    parsing probably complete

--- src/lingo_de.rs
+++ src/lingo_de.rs
@@ -6,7 +6,7 @@ use std::collections::HashMap;
 //todo: make sure support for negative numbers is not needed
 
 const REGEXSTR_PROPS: &str =
-    r#"\#(\w+):("[\w\d\s-]*?"|point\([\d,-]*?\)|\[\s*((,?\s*?(-?\d+|"[\w\d\s]*?"))*?)\s*\]|\d+)"#; // selects all flat properties from a tile serialization string. capture group 1 is property name and capture group 2 is property value (then fed to one of the lower regexes)
+    r#"\#(\w+):("[\\\w\d\s-]*?"|point\([\s\d,-]*?\)|\[\s*((\s*?,?\s*?(-?\d+|"[\w\d\s]*?"))*?)\s*\]|\d+)"#; // selects all flat properties from a tile serialization string. capture group 1 is property name and capture group 2 is property value (then fed to one of the lower regexes)
 const REGEXSTR_NUMBER: &str = r#"(-?\d+?)"#; //matches unsigned numbers. look at capture group 1 for contents
 const REGEXSTR_STRING: &str = r#""([\w\d\s]*?)""#; //matches "-delimited strings. look at capture group 1 for contents
 const REGEXSTR_ARRAY: &str = r#"\[(.*?)\]"#; //matches stuff in square brackets. look at capture group 1 for contents
@@ -49,9 +49,9 @@ impl LingoData {
             static ref REGEX_SPLITCOMMAS: regex::Regex =
                 regex::Regex::new(REGEXSTR_SPLITCOMMAS).unwrap();
         }
-        let mut res = Ok(LingoData::InvalidOrNull(text.to_string())); //Err(DeserError::RegexMatchFailed(text.to_string()));
-        if let Some(caps) = REGEX_ARRAY.captures(text) {
-            let spl = REGEX_SPLITCOMMAS.split(&caps[1]);
+        let mut res = Ok(LingoData::InvalidOrNull(text.to_string()));
+        if text.starts_with("[") && text.ends_with("]") {
+            let spl = REGEX_SPLITCOMMAS.split(&text[1..text.len() - 1]);
             res = Ok(Self::Array(
                 spl.into_iter()
                     .filter_map(|sub| match LingoData::parse(sub) {
@@ -60,8 +60,8 @@ impl LingoData {
                     })
                     .collect(),
             ))
-        } else if let Some(caps) = REGEX_POINT.captures(text) {
-            let spl = REGEX_SPLITCOMMAS.split(&caps[1]);
+        } else if text.starts_with("point(") && text.ends_with(")") {
+            let spl = REGEX_SPLITCOMMAS.split(&text[6..text.len() - 1]);
             res = Ok(Self::Point(
                 spl.into_iter()
                     .filter_map(|sub| match sub.parse::<i32>() {
@@ -70,17 +70,44 @@ impl LingoData {
                     })
                     .collect(),
             ))
-        } else if let Some(caps) = REGEX_STRING.captures(text) {
-            res = Ok(Self::String(String::from(&caps[1])))
-        } else if let Some(caps) = REGEX_NUMBER.captures(text) {
-            res = match &caps[1].parse::<i32>() {
-                Ok(num) => Ok(Self::Number(*num)),
-                Err(e) => Err(DeserError::ContentsNotParsed(format!(
-                    "{} (usize)",
-                    &caps[1]
-                ))),
-            }
+        } else if text.starts_with("\"") && text.ends_with("\"") {
+            res = Ok(LingoData::String(String::from(&text[1..text.len() - 1])))
+        } else if let Ok(val) = text.parse::<i32>() {
+            res = Ok(LingoData::Number(val))
         }
+
+        //Err(DeserError::RegexMatchFailed(text.to_string()));
+        // if let Some(caps) = REGEX_ARRAY.captures(text) {
+        //     let spl = REGEX_SPLITCOMMAS.split(&caps[1]);
+        //     res = Ok(Self::Array(
+        //         spl.into_iter()
+        //             .filter_map(|sub| match LingoData::parse(sub) {
+        //                 Ok(ld) => Some(Box::new(ld)),
+        //                 Err(_) => None,
+        //             })
+        //             .collect(),
+        //     ))
+        // } else if let Some(caps) = REGEX_POINT.captures(text) {
+        //     let spl = REGEX_SPLITCOMMAS.split(&caps[1]);
+        //     res = Ok(Self::Point(
+        //         spl.into_iter()
+        //             .filter_map(|sub| match sub.parse::<i32>() {
+        //                 Ok(num) => Some(num),
+        //                 Err(_) => None,
+        //             })
+        //             .collect(),
+        //     ))
+        // } else if let Some(caps) = REGEX_STRING.captures(text) {
+        //     res = Ok(Self::String(String::from(&caps[1])))
+        // } else if let Some(caps) = REGEX_NUMBER.captures(text) {
+        //     res = match &caps[1].parse::<i32>() {
+        //         Ok(num) => Ok(Self::Number(*num)),
+        //         Err(e) => Err(DeserError::ContentsNotParsed(format!(
+        //             "{} (usize)",
+        //             &caps[1]
+        //         ))),
+        //     }
+        // }
         res
     }
     pub fn as_number(&self) -> Result<i32, DeserError> {
